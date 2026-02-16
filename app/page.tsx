952cd05ed0a9629a7f7008c16a6a491d8cc3ea57
Mohamad Arleyxa Syarif <kiritonkichan@gmail.com>
2026-02-16 19:52:46 +0700

    experience isi

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import HeroSection from "@/components/Hero/HeroSection";
 import AboutSection from "@/components/About/AboutSection";
 import ServiceSection from "@/components/Service/ServiceSection";
 import ChooseSection from "@/components/Choose/ChooseSection";
+import ExperienceSection from "@/components/Experience/ExperienceSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
-      <ServiceSection />
       <ChooseSection />
+      <ServiceSection />
+      <ExperienceSection />
     </div>
   );
 }
