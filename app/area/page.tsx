5585393e0e15c5e7903e6877165bb70673deb0e6
Mohamad Arleyxa Syarif <kiritonkichan@gmail.com>
2026-02-25 22:03:45 +0700

    update cta

--- app/area/page.tsx
+++ app/area/page.tsx
@@ -1,6 +1,7 @@
 import AreaSection from "@/components/Area/AreaSection";
 import KotaArea from "./components/KotaArea";
 import HeroArea from "./components/HeroArea";
+import CtaSection from "@/components/Cta/CtaSection";
 
 export default function AreaPage() {
     return (
@@ -8,6 +9,7 @@ export default function AreaPage() {
             <HeroArea />
             <AreaSection />
             <KotaArea />
+            <CtaSection />
         </div>
     )
 }
